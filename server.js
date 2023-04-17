5cef9cae2103b3d181ef59ed00d4fcbf3eefe4b3
Herminio Henriques <herminiohenriques@gmail.com>
2023-04-17 17:40:20 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const colors = require("colors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+//env config
+dotenv.config();
+
+//router import
+// const userRoutes = require('./routes/userRoutes');
+// const blogRoutes = require('./routes/blogRoutes');
+
+//mongodb connection
+connectDB();
+
+//rest objecct
+const app = express();
+
+//middelwares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+//routes
+app.get("/", (req, res) => {
+  res.status(200).send({
+    message: "Node server",
+  });
+});
+
+// Port
+const PORT = process.env.PORT || 8080;
+
+//listen
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode port no ${PORT}`.bgCyan
+      .white
+  );
+});
